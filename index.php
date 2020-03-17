272bcd7a6f99fd076322d043a3bc1421f4727174
Cruz Ramirez <cruz_ramirez@ymail.com>
2020-03-17 10:56:21 -0700

    added dummy key

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php get_header(); ?>
+<?php
+
+include 'creds.php';
+
+get_header();
+?>
 
 	<main role="main">
 		<!-- section -->
@@ -7,6 +12,8 @@
 			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
 			<?php get_template_part('loop'); ?>
+
+			<h1><?php echo $API_key;?></h1>
 
 			<?php get_template_part('pagination'); ?>
 
